6bb7b827614ef3171cc0eb98a8f116cf9b5bf57d
dfsf dam <logicappsource@gmail.com>
2017-05-29 12:05:55 +0200

    BTC Wdiget UI

---
+++ BTC Widget/TodayViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TodayViewController.swift
+//  BTC Widget
+//
+//  Created by LogicAppSourceIO on 29/05/2017.
+//  Copyright © 2017 LogicAppSourceIO. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+import CryptoCurrencyKit
+
+
+class TodayViewController: CurrencyDataViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
--- Crypticker/AppDelegate.swift
+++ Crypticker/AppDelegate.swift
@@ -1,4 +1,6 @@
 
+
+
 import UIKit
 
 @UIApplicationMain
