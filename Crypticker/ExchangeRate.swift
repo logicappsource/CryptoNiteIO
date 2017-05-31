366252e8501f6329cb156bc264ab69410e288877
dfsf dam <logicappsource@gmail.com>
2017-05-31 14:55:44 +0200

    Initialize,  Fetching from API, in ETH Widget

--- Crypticker/ExchangeRate.swift
+++ Crypticker/ExchangeRate.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2017 LogicAppSourceIO. All rights reserved.
 //
 
+
+
 import Foundation
 import Serpent
 
@@ -13,52 +15,34 @@ import Serpent
 
 
 struct ExchangeRate {
-    var price = ""
-    var high24Hour = ""
-    var low24Hour = ""
-    var changePct24Hour = ""
-    var change24Hour = ""
-    var open24Hour = ""
-    var supply = ""
-    var lastVolume = ""
-    var volume24Hour = ""
-    var volume24HourTo = ""
-    var raw: [String : [String: AnyObject]] = [:]
+
+    var RAW: [String : [String: AnyObject]] = [:]
     
+}
+
+
+extension ExchangeRate: Serializable {
+    init(dictionary: NSDictionary?) {
+        RAW <== (self, dictionary, "RAW")
+    }
+    
+    func encodableRepresentation() -> NSCoding {
+        let dict = NSMutableDictionary()
+        (dict, "RAW") <== RAW
+        return dict
+    }
+}
+
+
+extension ExchangeRate {
+    var currencies: [CurrencyData]  {
+        return RAW.values.map({ (value) -> CurrencyData in
+            return CurrencyData(dictionary: value as NSDictionary)
+    })
 }
 
 
 
 
 
-extension ExchangeRate: Serializable {
-    init(dictionary: NSDictionary?) {
-        price           <== (self, dictionary, "price")
-        high24Hour      <== (self, dictionary, "high24Hour")
-        low24Hour       <== (self, dictionary, "low24Hour")
-        changePct24Hour <== (self, dictionary, "changePct24Hour")
-        change24Hour    <== (self, dictionary, "change24Hour")
-        open24Hour      <== (self, dictionary, "open24Hour")
-        supply          <== (self, dictionary, "supply")
-        lastVolume      <== (self, dictionary, "lastVolume")
-        volume24Hour    <== (self, dictionary, "volume24Hour")
-        volume24HourTo  <== (self, dictionary, "volume24HourTo")
-        raw             <== (self, dictionary, "raw")
-    }
-    
-    func encodableRepresentation() -> NSCoding {
-        let dict = NSMutableDictionary()
-        (dict, "price")           <== price
-        (dict, "high24Hour")      <== high24Hour
-        (dict, "low24Hour")       <== low24Hour
-        (dict, "changePct24Hour") <== changePct24Hour
-        (dict, "change24Hour")    <== change24Hour
-        (dict, "open24Hour")      <== open24Hour
-        (dict, "supply")          <== supply
-        (dict, "lastVolume")      <== lastVolume
-        (dict, "volume24Hour")    <== volume24Hour
-        (dict, "volume24HourTo")  <== volume24HourTo
-        (dict, "raw")             <== raw
-        return dict
-    }
 }
---
+++ ETH Widget/TodayEthViewController.swift
@@ -0,0 +1,86 @@
+//
+//  TodayEthViewController.swift
+//  ETH Widget
+//
+//  Created by LogicAppSourceIO on 31/05/2017.
+//  Copyright © 2017 LogicAppSourceIO. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+import CryptoCurrencyKit
+import Serpent
+import Alamofire
+
+
+class TodayEthViewController: CurrencyDataViewController, NCWidgetProviding {
+    
+     let baseURL_Price = URL (string: "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH,DASH,XRP&tsyms=BTC,USD,EURO")! //Add parameters in the url for more data request
+    
+    @IBOutlet weak var ethPriceLbl: UILabel!
+    
+    @IBOutlet weak var ethPriceChangeLbl: UILabel!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+        
+    
+        requestEthereumPrice { (response) in
+            switch response.result {
+                
+            case .success (let exchangeRate):
+                
+               print("Result of the respond API: \(exchangeRate.RAW)")
+                
+               for currency in exchangeRate.currencies {
+                    print("Currenct XRP \(exchangeRate.currencies)")
+                }
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+                
+            }
+        }
+        
+    }
+    
+    
+    /* API data respond
+     if let Ethereum = currency.eth["USD"] as? Dictionary <String, Any> {
+     print(Ethereum)
+     }
+     //print("Currency Data from ETHEREUM: \(currency.eth)")
+     //print("Currency Data from DASH: \(currency.change24Hour)")
+     */
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+    
+    
+    //GET Ethereum Endpoint - API
+    func requestEthereumPrice(completion: @escaping (DataResponse<ExchangeRate>) -> Void) {
+        request(baseURL_Price).responseSerializable(completion)
+        
+    }
+    
+    
+    
+    
+}
